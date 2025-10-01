ed2cb0b2915bdd538256dfebd83a9fa108aa51a7
Nicholas Crump <nickacrump@gmail.com>
2025-10-01 15:53:58 -0400

    Finished Selenium Script and Github Action

--- .automatedTests/v1_0_0_NavigationTest.py
+++ .automatedTests/v1_0_0_NavigationTest.py
@@ -13,7 +13,7 @@ import time
 # python ./.automatedTests/v1_0_0_NavigationTest.py
 
 chrome_options = Options()
-#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
