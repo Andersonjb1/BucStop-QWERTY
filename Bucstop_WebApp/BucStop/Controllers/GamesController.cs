49068c6a0ecff41c7aef490b30ff80298ccab886
T0xen <jl.graham5002@gmail.com>
2025-11-05 20:56:17 -0500

    changed file username

--- Bucstop_WebApp/BucStop/Controllers/GamesController.cs
+++ Bucstop_WebApp/BucStop/Controllers/GamesController.cs
@@ -163,7 +163,7 @@ namespace BucStop.Controllers
 
                 // Create a unique filename: username + timestamp
                 // \var safeUsername = string.IsNullOrWhiteSpace(username) ? "anonymous" : username;
-                var uniqueFileName = $"{safeUsername}_{DateTime.UtcNow:yyyyMMdd_HHmmss}{fileExtension}";
+                var uniqueFileName = $"{submissionModel.Username}_{DateTime.UtcNow:yyyyMMdd_HHmmss}{fileExtension}";
 
                 // Full path inside container (which maps to the Docker volume)
                 var filePath = Path.Combine(submissionDirectory, uniqueFileName);
