f2ba7dc341a33fbe3ea1a633098f51c74a9637b7
T0xen <jl.graham5002@gmail.com>
2025-11-20 01:48:36 -0500

    Moved Snapshots folder out of Admin, there's probably a way to get it to work in it but it jut simply won't not show the error page whenever I navigate to the page. Admin and Snapshot only appear when logged in as an admin and you can logout on those pages.

--- Bucstop_WebApp/BucStop/Controllers/AdminController.cs
+++ Bucstop_WebApp/BucStop/Controllers/AdminController.cs
@@ -1,7 +1,16 @@
-using BucStop.Models;
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BucStop.Models;
+using BucStop.Services;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Net.Http;
 using Microsoft.AspNetCore.Authorization;
+
+
 
 /*
  * This file has the controllers for everything outside of the games
@@ -14,15 +23,27 @@ namespace BucStop.Controllers
     {
         private readonly ILogger<AdminController> _logger;
         private readonly MicroClient _httpClient;
+        private readonly SnapshotService _snapshotService;
+        private readonly PlayCountManager _playCountManager;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IHostEnvironment _host;
 
-        public AdminController(MicroClient microClient, ILogger<AdminController> logger)
+        public AdminController(
+            MicroClient microClient,
+            ILogger<AdminController> logger,
+            SnapshotService snapshotService,
+            IWebHostEnvironment webHostEnvironment,
+            IHostEnvironment host)
         {
-            _logger = logger;
+            _snapshotService = snapshotService;
             _httpClient = microClient;
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+            _playCountManager = new PlayCountManager(_httpClient.GetGamesList() ?? new List<Game>(), webHostEnvironment);
+            _host = host;
         }
-         
 
-        //Takes the user to the admin page.
+        // Takes the user to the admin page.
         [Authorize]
         public IActionResult Index()
         {
@@ -30,7 +51,7 @@ namespace BucStop.Controllers
             return View(_httpClient.GetGamesList());
         }
 
-        //If something goes wrong, this will take the user to a page explaining the error.
+        // If something goes wrong, this will take the user to a page explaining the error.
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
