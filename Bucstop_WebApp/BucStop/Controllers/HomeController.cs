fd47afb254da59b0809002605c1d5e3fb1290e1f
T0xen <jl.graham5002@gmail.com>
2025-11-20 13:19:23 -0500

    Added AdminController.cs, made it so that Snapshots requires authorization, and removed Admin reference from homecontroller (still need to move file)

--- Bucstop_WebApp/BucStop/Controllers/HomeController.cs
+++ Bucstop_WebApp/BucStop/Controllers/HomeController.cs
@@ -14,28 +14,17 @@ namespace BucStop.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly MicroClient _httpClient;
-        private readonly SubmissionClient _submissionClient;
 
-        public HomeController(MicroClient microClient, SubmissionClient submissionClient, ILogger<HomeController> logger)
+        public HomeController(MicroClient microClient, ILogger<HomeController> logger)
         {
             _logger = logger;
             _httpClient = microClient;
-            _submissionClient = submissionClient;
         }
 
         //Sends the user to the deprecated Index page.
         public async Task<IActionResult> Index()
         {
             return View(_httpClient.GetGamesList());
-        }
-
-        //Takes the user to the admin page.
-        [Authorize]
-        public IActionResult Admin()
-        {
-            _logger.LogInformation("{Category}: {User} visited the Admin page.", "UserActivity", User.Identity?.Name ?? "Anonymous");
-            return View(_submissionClient.GetSubmissionsList());
-
         }
 
         //Takes the user to the about policy page.
